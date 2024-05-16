5664c11de11a7841733ab53e69940a58437571dd
prane12 <pranesh9096@gmail.com>
2024-05-16 21:23:01 +0530

    adding the files

---
+++ main.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_ecs_cluster" "hello_world_cluster" {
+  name = "hello-world-cluster"
+}
+
+resource "aws_ecs_task_definition" "hello_world_task" {
+  family                   = "hello-world-task"
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+  requires_compatibilities = ["FARGATE"]
+
+  container_definitions = jsonencode([
+    {
+      name      = "hello-world-container"
+      image     = "public.ecr.aws/y9r8c7u6/nodejs-app:latest"
+      cpu       = 256
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 3000
+          hostPort      = 3000
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "hello_world_service" {
+  name            = "hello-world-service"
+  cluster         = aws_ecs_cluster.hello_world_cluster.id
+  task_definition = aws_ecs_task_definition.hello_world_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = ["subnet-046466961d8f5ce45", "subnet-02817fde507f1060f"] // Subnet IDs
+    security_groups = ["sg-046d3f7a2fb646c86"] // Security Group IDs
+    assign_public_ip = true
+  }
+}
